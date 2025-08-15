e79e1af2a62a26df1159deeaa2170f63de9dbb38
joschonarth <joschonarth@gmail.com>
2025-08-15 16:38:27 -0300

    chore: setup env variables

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
-import { app } from "./app.js";
+import { app } from "./app";
+import { env } from "./env";
 
 app.listen({
   host: '0.0.0.0',
-  port: 3333
+  port: env.PORT
 }).then(() => {
-  console.log('✅ Server running at http://localhost:3333')
+  console.log(`✅ Server running at http://localhost:${env.PORT}`)
 })
