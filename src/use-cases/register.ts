35290033680a75c881b583852fd0f01450408479
joschonarth <joschonarth@gmail.com>
2025-08-16 16:56:59 -0300

    feat: implement users repository

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,6 +1,6 @@
 import { hash } from 'bcrypt'
 
-import { prisma } from '@/lib/prisma'
+import { UsersRepository } from '@/repositories/users.repository'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -8,28 +8,22 @@ interface RegisterUseCaseRequest {
   password: string
 }
 
-export async function registerUseCase({
-  name,
-  email,
-  password,
-}: RegisterUseCaseRequest) {
-  const passwordHash = await hash(password, 6)
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) {}
 
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    const passwordHash = await hash(password, 6)
 
-  if (userWithSameEmail) {
-    throw new Error('E-mail already exists.')
-  }
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
-  await prisma.user.create({
-    data: {
+    if (userWithSameEmail) {
+      throw new Error('E-mail already exists.')
+    }
+
+    await this.usersRepository.create({
       name,
       email,
       password: passwordHash,
-    },
-  })
+    })
+  }
 }
