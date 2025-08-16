a57a432386a2bf54d3ed5372c471774c9cd17188
joschonarth <joschonarth@gmail.com>
2025-08-16 19:05:06 -0300

    feat: implement authenticate controller

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify'
 
+import { authenticate } from '@/controllers/authenticate.controller'
 import { register } from '@/controllers/register.controller'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
+  app.post('/sessions', authenticate)
 }
