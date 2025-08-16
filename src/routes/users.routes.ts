0b29cd89f4de3edfb027d18acd3aed5e5cefe015
joschonarth <joschonarth@gmail.com>
2025-08-16 15:39:34 -0300

    feat: implement register use case

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from 'fastify'
 
-import { register } from '@/controllers/register'
+import { register } from '@/controllers/register.controller'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
---
+++ src/use-cases/register.ts
@@ -0,0 +1,35 @@
+import { hash } from 'bcrypt'
+
+import { prisma } from '@/lib/prisma'
+
+interface RegisterUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export async function registerUseCase({
+  name,
+  email,
+  password,
+}: RegisterUseCaseRequest) {
+  const passwordHash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    throw new Error('E-mail already exists.')
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: passwordHash,
+    },
+  })
+}
