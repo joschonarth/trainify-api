027b9fa36fab73229ce92c1be64a3f62f69d88c0
joschonarth <joschonarth@gmail.com>
2025-08-29 23:06:40 -0300

    feat: implement get exercise log use case

---
+++ src/use-cases/exercise-logs/get-exercise-log.use-case.ts
@@ -0,0 +1,35 @@
+import { ExerciseLog } from '@prisma/client'
+
+import { ResourceNotFoundError } from '@/errors/resource-not-found.error'
+import { ExerciseLogsRepository } from '@/repositories/exercise-logs.repository'
+
+interface GetExerciseLogUseCaseRequest {
+  logId: string
+}
+
+interface GetExerciseLogUseCaseResponse {
+  log: ExerciseLog & {
+    exercise: {
+      id: string
+      name: string
+      category: string | null
+      type: string | null
+    }
+  }
+}
+
+export class GetExerciseLogUseCase {
+  constructor(private exerciseLogsRepository: ExerciseLogsRepository) {}
+
+  async execute({
+    logId,
+  }: GetExerciseLogUseCaseRequest): Promise<GetExerciseLogUseCaseResponse> {
+    const log = await this.exerciseLogsRepository.findById(logId)
+
+    if (!log) {
+      throw new ResourceNotFoundError('Exercise log not found.')
+    }
+
+    return { log }
+  }
+}
