f1e4d2cb7c7388c02d7201fa78e2f7f2e7822795
joschonarth <joschonarth@gmail.com>
2025-08-29 22:49:33 -0300

    feat: implement fetch exercise logs use case

--- src/repositories/exercise-logs.repository.ts
+++ src/repositories/exercise-logs.repository.ts
@@ -2,6 +2,17 @@ import { ExerciseLog, Prisma } from '@prisma/client'
 
 export interface ExerciseLogsRepository {
   create(data: Prisma.ExerciseLogCreateInput): Promise<ExerciseLog>
+
+  findAllByUser(userId: string): Promise<
+    (ExerciseLog & {
+      exercise: {
+        id: string
+        name: string
+        category: string | null
+        type: string | null
+      }
+    })[]
+  >
   findByUserAndExerciseBetweenDates(
     userId: string,
     exerciseId: string,
