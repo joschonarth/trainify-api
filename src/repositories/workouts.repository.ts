352971f8ca4df639dab0a753ba2aa5ce0e31d494
joschonarth <joschonarth@gmail.com>
2025-08-31 16:05:29 -0300

    feat: implement fetch user workouts use case

--- src/repositories/workouts.repository.ts
+++ src/repositories/workouts.repository.ts
@@ -2,4 +2,25 @@ import { Prisma, Workout } from '@prisma/client'
 
 export interface WorkoutsRepository {
   create(data: Prisma.WorkoutCreateInput): Promise<Workout>
+  findAllByUser(userId: string): Promise<
+    (Workout & {
+      exercises: {
+        id: string
+        exerciseId: string
+        defaultSets: number | null
+        defaultReps: number | null
+        defaultWeight: number | null
+        exercise: {
+          id: string
+          name: string
+          category: string | null
+          type: string | null
+        }
+      }[]
+      schedules: {
+        id: string
+        dayOfWeek: number
+      }[]
+    })[]
+  >
 }
