1aaf66eaa04b418c236f3413655a0e02fc03e465
joschonarth <joschonarth@gmail.com>
2025-08-31 22:55:31 -0300

    refactor: adjust make get workout details file name and imports

--- src/use-cases/workouts/factories/make-get-workout-details-use-case.ts
+++ src/use-cases/workouts/factories/make-get-workout-details-use-case.ts
@@ -1,6 +1,6 @@
 import { PrismaWorkoutsRepository } from '@/repositories/prisma/prisma-workouts.repository'
 
-import { GetWorkoutDetailsUseCase } from '../get-workout-details'
+import { GetWorkoutDetailsUseCase } from '../get-workout-details.use-case'
 
 export function makeGetWorkoutDetailsUseCase() {
   const workoutsRepository = new PrismaWorkoutsRepository()
