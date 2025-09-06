5c7371c5746361801ab4317b9a1186a6535084e8
joschonarth <joschonarth@gmail.com>
2025-09-06 12:49:48 -0300

    feat: implement fetch user schedules use case

---
+++ src/use-cases/users/fetch-user-schedules.use-case.ts
@@ -0,0 +1,62 @@
+import { ResourceNotFoundError } from '@/errors/resource-not-found.error'
+import { UsersRepository } from '@/repositories/users.repository'
+import { WorkoutsRepository } from '@/repositories/workouts.repository'
+
+interface FetchUserSchedulesUseCaseRequest {
+  userId: string
+}
+
+interface FetchUserSchedulesUseCaseResponse {
+  schedules: {
+    dayOfWeek: number
+    workout: {
+      id: string
+      name: string
+      exercises: {
+        id: string
+        name: string
+        sets: number | null
+        reps: number | null
+        weight: number | null
+      }[]
+    }
+  }[]
+}
+
+export class FetchUserSchedulesUseCase {
+  constructor(
+    private usersRepository: UsersRepository,
+    private workoutsRepository: WorkoutsRepository,
+  ) {}
+
+  async execute({
+    userId,
+  }: FetchUserSchedulesUseCaseRequest): Promise<FetchUserSchedulesUseCaseResponse> {
+    const user = await this.usersRepository.findById(userId)
+
+    if (!user) {
+      throw new ResourceNotFoundError()
+    }
+
+    const workouts = await this.workoutsRepository.findAllByUser(userId)
+
+    const schedules = workouts.flatMap((workout) =>
+      workout.schedules.map((schedule) => ({
+        dayOfWeek: schedule.dayOfWeek,
+        workout: {
+          id: workout.id,
+          name: workout.name,
+          exercises: workout.exercises.map((we) => ({
+            id: we.exercise.id,
+            name: we.exercise.name,
+            sets: we.defaultSets,
+            reps: we.defaultReps,
+            weight: we.defaultWeight,
+          })),
+        },
+      })),
+    )
+
+    return { schedules }
+  }
+}
