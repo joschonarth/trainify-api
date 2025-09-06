d13f7311abdc6927e3b2c16e55f94f7e8293c791
joschonarth <joschonarth@gmail.com>
2025-09-06 12:18:52 -0300

    feat: implement fetch workout schedules controller

--- src/routes/workouts.routes.ts
+++ src/routes/workouts.routes.ts
@@ -6,6 +6,7 @@ import { createWorkoutController } from '@/controllers/workouts/create-workout.c
 import { deleteWorkoutController } from '@/controllers/workouts/delete-workout.controller'
 import { fetchUserWorkoutsController } from '@/controllers/workouts/fetch-user-workouts.controller'
 import { fetchWorkoutExercisesController } from '@/controllers/workouts/fetch-workout-exercises.controller'
+import { fetchWorkoutSchedulesController } from '@/controllers/workouts/fetch-workout-schedules.controller'
 import { getWorkoutDetailsController } from '@/controllers/workouts/get-workout-details'
 import { removeExerciseFromWorkoutController } from '@/controllers/workouts/remove-exercise-from-workout.controller'
 import { updateWorkoutController } from '@/controllers/workouts/update-workout.controller'
@@ -19,6 +20,7 @@ export async function workoutsRoutes(app: FastifyInstance) {
   app.get('/workouts', fetchUserWorkoutsController)
   app.get('/workouts/:workoutId', getWorkoutDetailsController)
   app.get('/workouts/:workoutId/exercises', fetchWorkoutExercisesController)
+  app.get('/workouts/:workoutId/schedules', fetchWorkoutSchedulesController)
 
   app.post('/workouts', createWorkoutController)
   app.post('/workouts/:workoutId/exercises', addExerciseToWorkoutController)
