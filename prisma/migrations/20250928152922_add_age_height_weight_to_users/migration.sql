41a2c39f8e70fa66a11cecd7868360da3f584bea
joschonarth <joschonarth@gmail.com>
2025-09-28 15:11:27 -0300

    feat: implement update user profile use case

---
+++ prisma/migrations/20250928152922_add_age_height_weight_to_users/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN "age" INTEGER;
+ALTER TABLE "users" ADD COLUMN "height" INTEGER;
+ALTER TABLE "users" ADD COLUMN "weight" REAL;
---
+++ src/use-cases/users/update-user-profile.use-case.ts
@@ -0,0 +1,47 @@
+import { User } from '@prisma/client'
+
+import { ResourceNotFoundError } from '@/errors/resource-not-found.error'
+import { UsersRepository } from '@/repositories/users.repository'
+
+interface UpdateUserProfileUseCaseRequest {
+  userId: string
+  name?: string
+  age?: number
+  height?: number
+  weight?: number
+}
+
+interface UpdateUserProfileUseCaseResponse {
+  user: User
+}
+
+export class UpdateUserProfileUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    userId,
+    name,
+    age,
+    height,
+    weight,
+  }: UpdateUserProfileUseCaseRequest): Promise<UpdateUserProfileUseCaseResponse> {
+    const user = await this.usersRepository.findById(userId)
+
+    if (!user) {
+      throw new ResourceNotFoundError()
+    }
+
+    const updatedUser = await this.usersRepository.update(
+      userId,
+      Object.fromEntries(
+        Object.entries({ name, age, height, weight }).filter(
+          ([, value]) => value !== undefined,
+        ),
+      ),
+    )
+
+    return {
+      user: updatedUser,
+    }
+  }
+}
