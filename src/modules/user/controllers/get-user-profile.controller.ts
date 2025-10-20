6828502e171678a92b0a94ec0a8ac6c29c1f2549
joschonarth <joschonarth@gmail.com>
2025-10-19 22:53:14 -0300

    feat: handle resource not found error in get user profile controller

--- src/modules/user/controllers/get-user-profile.controller.ts
+++ src/modules/user/controllers/get-user-profile.controller.ts
@@ -1,21 +1,32 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 import { makeGetUserProfileUseCase } from '@/modules/user/use-cases/factories/make-get-user-profile-use-case'
+import { ResourceNotFoundError } from '@/shared/errors/resource-not-found.error'
 
 export async function getUserProfileController(
   request: FastifyRequest,
   reply: FastifyReply,
 ) {
-  const getUserProfile = makeGetUserProfileUseCase()
+  try {
+    const getUserProfile = makeGetUserProfileUseCase()
 
-  const { user } = await getUserProfile.execute({
-    userId: request.user.userId,
-  })
+    const { user } = await getUserProfile.execute({
+      userId: request.user.userId,
+    })
 
-  return reply.status(200).send({
-    user: {
-      ...user,
-      password: undefined,
-    },
-  })
+    return reply.status(200).send({
+      user: {
+        ...user,
+        password: undefined,
+      },
+    })
+  } catch (error) {
+    if (error instanceof ResourceNotFoundError) {
+      return reply.status(404).send({
+        message: error.message,
+      })
+    }
+
+    throw error
+  }
 }
