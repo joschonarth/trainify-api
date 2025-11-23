d8c6ceb42a066eff60a9849f971bb892d1bbb086
joschonarth <joschonarth@gmail.com>
2025-11-23 12:26:12 -0300

    feat: make automatic authentication when creating an account

--- src/modules/user/controllers/register.controller.ts
+++ src/modules/user/controllers/register.controller.ts
@@ -1,6 +1,7 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
+import { env } from '@/env'
 import { PasswordsDoNotMatchError } from '@/modules/user/errors/passwords-do-not-match.error'
 import { UserAlreadyExistsError } from '@/modules/user/errors/user-already-exists.error'
 import { makeRegisterUseCase } from '@/modules/user/use-cases/factories/make-register-use-case'
@@ -22,11 +23,34 @@ export async function registerController(
   try {
     const registerUseCase = makeRegisterUseCase()
 
-    await registerUseCase.execute({
+    const { user } = await registerUseCase.execute({
       name,
       email,
       password,
       passwordConfirmation,
+    })
+
+    const token = await reply.jwtSign(
+      {
+        userId: user.id,
+        email: user.email,
+      },
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+
+    reply.setCookie('token', token, {
+      path: '/',
+      httpOnly: true,
+      secure: env.NODE_ENV === 'production',
+      sameSite: 'strict',
+    })
+
+    return reply.status(201).send({
+      token,
     })
   } catch (error) {
     if (error instanceof UserAlreadyExistsError) {
@@ -43,6 +67,4 @@ export async function registerController(
 
     throw error
   }
-
-  return reply.status(201).send()
 }
