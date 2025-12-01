7128375d9e18269d0355d6a4ba7ac0d9d47815aa
joschonarth <joschonarth@gmail.com>
2025-12-01 03:00:27 -0300

    fix: remove migrations for deploy

--- prisma/migrations/20251201003835_add_unique_to_workouts/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[name,userId]` on the table `workouts` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "workouts_name_userId_key" ON "workouts"("name", "userId");
--- prisma/migrations/20251201004531_add_seed_state_table/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateTable
-CREATE TABLE "SeedState" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "executed" BOOLEAN NOT NULL DEFAULT false,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
