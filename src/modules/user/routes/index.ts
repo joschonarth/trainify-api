feab0567e6f9be055720a8f745184c0c21a1f243
joschonarth <joschonarth@gmail.com>
2026-03-16 18:29:46 -0300

    docs(user): document register route with Swagger

--- src/modules/user/routes/index.ts
+++ src/modules/user/routes/index.ts
@@ -1,6 +1,20 @@
 import type { FastifyInstance } from 'fastify'
 import { authenticateRoute } from './authenticate.route'
+import { changePasswordRoute } from './change-password.route'
+import { fetchUserSchedulesRoute } from './fetch-user-schedules.route'
+import { getUserProfileRoute } from './get-user-profile.route'
+import { registerRoute } from './register.route'
+import { signInWithGoogleRoute } from './sign-in-with-google.route'
+import { signOutRoute } from './sign-out.route'
+import { updateUserProfileRoute } from './update-user-profile.route'
 
 export function usersRoutes(app: FastifyInstance) {
+  app.register(registerRoute)
   app.register(authenticateRoute)
+  app.register(signInWithGoogleRoute)
+  app.register(getUserProfileRoute)
+  app.register(fetchUserSchedulesRoute)
+  app.register(updateUserProfileRoute)
+  app.register(changePasswordRoute)
+  app.register(signOutRoute)
 }
