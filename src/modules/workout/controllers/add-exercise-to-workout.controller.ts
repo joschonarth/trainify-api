cb9598b3997d3970ab6a97b9dd3113c686b0d78d
joschonarth <joschonarth@gmail.com>
2026-03-31 20:37:15 -0300

    docs(workout): document add exercise to workout route

--- src/modules/workout/controllers/add-exercise-to-workout.controller.ts
+++ src/modules/workout/controllers/add-exercise-to-workout.controller.ts
@@ -1,31 +1,22 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
-import { z } from 'zod'
 
 import { ResourceAlreadyExistsError } from '@/shared/errors/resource-already-exists.error'
 import { ResourceNotFoundError } from '@/shared/errors/resource-not-found.error'
-
+import type {
+  AddExerciseToWorkoutBody,
+  AddExerciseToWorkoutParams,
+} from '../schemas/add-exercise-to-workout.schema'
 import { makeAddExerciseToWorkoutUseCase } from '../use-cases/factories/make-add-exercise-to-workout-use-case'
 
 export async function addExerciseToWorkoutController(
   request: FastifyRequest,
   reply: FastifyReply
 ) {
-  const bodySchema = z.object({
-    exerciseId: z.string(),
-    defaultSets: z.number().nullable(),
-    defaultReps: z.number().nullable(),
-    defaultWeight: z.number().nullable(),
-  })
-
-  const paramsSchema = z.object({
-    workoutId: z.string(),
-  })
+  const { workoutId } = request.params as AddExerciseToWorkoutParams
+  const { exerciseId, defaultSets, defaultReps, defaultWeight } =
+    request.body as AddExerciseToWorkoutBody
 
   try {
-    const { exerciseId, defaultSets, defaultReps, defaultWeight } =
-      bodySchema.parse(request.body)
-    const { workoutId } = paramsSchema.parse(request.params)
-
     const addExerciseToWorkoutUseCase = makeAddExerciseToWorkoutUseCase()
     const { workoutExercise } = await addExerciseToWorkoutUseCase.execute({
       workoutId,
