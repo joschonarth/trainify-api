6c60e535ce65b9489bdaac1d74975a94deac83f4
joschonarth <joschonarth@gmail.com>
2026-04-01 23:33:01 -0300

    docs(weight): document update weight log route

--- src/modules/weight/controllers/update-weight-log.controller.ts
+++ src/modules/weight/controllers/update-weight-log.controller.ts
@@ -1,28 +1,21 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
-import { z } from 'zod'
 
 import { ResourceNotFoundError } from '@/shared/errors/resource-not-found.error'
-
+import type {
+  UpdateWeightLogBody,
+  UpdateWeightLogParams,
+} from '../schemas/update-weight-log.schema'
 import { makeUpdateWeightLogUseCase } from '../use-cases/factories/make-update-weight-log-use-case'
 
 export async function updateWeightLogController(
   request: FastifyRequest,
   reply: FastifyReply
 ) {
+  const { logId } = request.params as UpdateWeightLogParams
+  const { weight, note } = request.body as UpdateWeightLogBody
+  const userId = request.user.sub
+
   try {
-    const paramsSchema = z.object({
-      logId: z.string(),
-    })
-
-    const bodySchema = z.object({
-      weight: z.number().optional(),
-      note: z.string().nullable().optional(),
-    })
-
-    const { logId } = paramsSchema.parse(request.params)
-    const { weight, note } = bodySchema.parse(request.body)
-    const userId = request.user.sub
-
     const updateWeightLogUseCase = makeUpdateWeightLogUseCase()
     const updatedLog = await updateWeightLogUseCase.execute({
       logId,
@@ -31,7 +24,7 @@ export async function updateWeightLogController(
       note,
     })
 
-    return reply.status(200).send(updatedLog)
+    return reply.status(200).send({ updatedLog })
   } catch (error) {
     if (error instanceof ResourceNotFoundError) {
       return reply.status(404).send({ message: error.message })
